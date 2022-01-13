8ca37afefa8da2dd9b7b93e85f2fdc936115afb5
md-alramlawi <git2021@leen>
2022-01-13 02:19:06 +0200

    update gradle

--- app/src/main/java/com/ramlawi/crypto/common/Constants.kt
+++ app/src/main/java/com/ramlawi/crypto/common/Constants.kt
@@ -7,4 +7,5 @@ package com.ramlawi.crypto.common
 object Constants {
     const val BASE_URL = "https://api.coinpaprika.com/"
     const val PARAM_COIN_ID = "coinId"
+    const val PARAM_COIN = "coinId55"
 }
